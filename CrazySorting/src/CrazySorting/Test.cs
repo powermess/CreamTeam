9a85327e6027136ca851a9c6775bf7976ac14868
p m <weathered.woods@gmail.com>
2017-02-04 15:36:39 +0100

    restructure everything

---
+++ CrazySorting/src/CrazySorting/Test.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace CrazySorting
+{
+    class Test : MonoBehaviour
+    {
+        void Awake()
+        {
+#if UNITY_EDITOR
+            Debug.Log("Editor");
+#elif UNITY_ANDROID
+            Debug.Log("Android");
+#endif
+        }
+    }
+}
