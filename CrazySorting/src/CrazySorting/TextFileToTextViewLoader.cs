a3bef935fff7c5038c39bb797567525fa6c819ae
p m <weathered.woods@gmail.com>
2017-02-04 20:43:02 +0100

    added changelog

---
+++ CrazySorting/src/CrazySorting/TextFileToTextViewLoader.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace CrazySorting
+{
+
+    class ChangeLogBehaviour : MonoBehaviour
+    {
+        public Text ChangeLogUIText;
+        public TextAsset ChangeLog;
+
+        void Awake()
+        {
+            var text = ChangeLog.text;
+
+            if (text.Length == 0 || PlayerPrefs.GetInt("ChangeLogLength") == text.Length)
+            {
+                Dismiss();
+                return;
+            }
+
+            ChangeLogUIText.text = ChangeLog.text;
+
+            PlayerPrefs.SetInt("ChangeLogLength", text.Length);
+        }
+
+        public void Dismiss()
+        {
+            DestroyImmediate(this.gameObject);
+        }
+    }
+}
