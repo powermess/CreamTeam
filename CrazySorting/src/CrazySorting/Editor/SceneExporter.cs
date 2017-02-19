63d22eaed4700b50caf894054de583409e7f2ed6
Peter Messenger <messenger.mine@gmail.com>
2017-02-19 23:26:12 +0100

    Added level selector

---
+++ CrazySorting/src/CrazySorting/Editor/SceneExporter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+class SceneExporter : Editor
+{
+    [MenuItem("CreamTeam/ExportLevelNames")]
+    public static void ExportLevelNames()
+    {
+        var scenes = new List<string>();
+        foreach (var scene in EditorBuildSettings.scenes.Where(s => s.enabled))
+        {
+            scenes.Add(Path.GetFileNameWithoutExtension(scene.path));
+        }
+
+        var fileName = Application.dataPath + "/Resources/scene_names.txt";
+        if (!File.Exists(fileName))
+        {
+            var file = File.Create(fileName);
+            file.Close();
+        }
+        File.WriteAllLines(fileName, scenes.ToArray());
+    }
+}
---
+++ CrazySorting/src/CrazySorting/LevelSelector.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace CrazySorting
+{
+    class LevelSelector : MonoBehaviour
+    {
+        IList<string> mScenes = new List<string>();
+
+        private void Awake()
+        {
+            var sceneTextAsset = Resources.Load<TextAsset>("scene_names");
+            mScenes = sceneTextAsset.text.Split('\n')
+                .Where(s=>!string.IsNullOrEmpty(s) && s != "LevelSelectorScreen")
+                .ToList();
+        }
+
+        private void OnGUI()
+        {
+            var yPos = 0f;
+            var height = Screen.height / 10f;
+            var xPadding = 5;
+            var yPadding = 5;
+
+            foreach(var scene in mScenes)
+            {
+                if (GUI.Button(new Rect(new Vector2(xPadding, yPos + yPadding), new Vector2(Screen.width - 2 * xPadding, height)), scene))
+                {
+                    SceneManager.LoadScene(scene);
+                }
+
+                yPos += height + yPadding;
+            }
+        }
+    }
+
+    
+}
